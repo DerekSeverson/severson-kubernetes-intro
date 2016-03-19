15bb8e2f5a047be93fe85f0468a78496de5c627b
Derek Severson <programmer.severson@gmail.com>
2016-03-19 12:42:16 -0500

    Create simple nodejs server

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
